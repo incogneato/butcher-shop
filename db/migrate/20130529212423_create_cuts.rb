92efeeddbfedcb28881fee0d5b707563fc11810c
Wiggs <mwiggins@book.com>
2013-06-02 22:10:23 -0700

    modify cuts migration to add down method, because it seemed like a good idea at the time.

--- db/migrate/20130529212423_create_cuts.rb
+++ db/migrate/20130529212423_create_cuts.rb
@@ -1,5 +1,5 @@
 class CreateCuts < ActiveRecord::Migration
-  def change
+  def up
     create_table :cuts do |t|
       t.string :name
       t.references :primal_cut
@@ -9,5 +9,12 @@ class CreateCuts < ActiveRecord::Migration
     end
     add_index :cuts, :primal_cut_id
     add_index :cuts, :animal_id
+  end
+
+  def down
+    drop_table :cuts
+
+    remove_index :cuts, :primal_cut_id
+    remove_index :cuts, :animal_id
   end
 end
