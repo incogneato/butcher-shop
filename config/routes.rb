2c12f30845acafd74ccab95ff77d942eae289c8b
Wiggs <mwiggins@book.com>
2013-06-02 21:58:18 -0700

    add respective routes for favorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 ButcherShop::Application.routes.draw do
-  resources :users, :except => [:new, :edit]
+  resources :users, :except => [:new, :edit] do
+    resources :favorites, :only => [:index, :create]
+  end
+
+  resources :favorites, :only => [:show, :update, :destroy]
 
   resources :animals, :except => [:new, :edit] do
     resources :primal_cuts, :only => [:index, :create]
